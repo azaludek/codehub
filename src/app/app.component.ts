ae46cc4a3e82f36853b4749a8c57d9296ab83d33
Adam Žalůdek <104594328+azaludek@users.noreply.github.com>
2024-03-12 21:14:51 +0100

    Add: signup & login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { initFlowbite } from 'flowbite';
 import {NavbarComponent} from "./components/navbar/navbar.component";
 import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
 import {FooterComponent} from "./components/footer/footer.component";
+import {SignupComponent} from "./pages/signup/signup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LandingPageComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, LandingPageComponent, FooterComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+// app.routes.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './pages/signup/signup.component';
+import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
+import {LoginComponent} from "./pages/login/login.component";
 
-export const routes: Routes = [];
+// Define your routes
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent }
+];
+
